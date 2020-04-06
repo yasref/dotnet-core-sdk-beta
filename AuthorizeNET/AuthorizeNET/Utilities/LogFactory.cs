feb2a4323257eaa85e4ce55930fae593bdf20603
Yaseen Alrefaee <yaseenref@gmail.com>
2020-04-06 07:54:20 +0300

    Using log4net

--- AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
+++ AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
@@ -5,7 +5,7 @@
 
     public static class LogFactory
     {
-        private static ILoggerFactory LoggerFactory => new LoggerFactory().AddDebug(LogLevel.Debug);
+        private static ILoggerFactory LoggerFactory => new LoggerFactory().AddLog4Net();
 
         public static ILogger getLog(Type classType)
         {
